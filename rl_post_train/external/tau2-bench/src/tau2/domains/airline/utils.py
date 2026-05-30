3eba4adc65948c9b11cab6000be7ad1a3909df7f
KoSpades <steven.sy.xia@gmail.com>
2026-05-29 23:55:51 -0500

    test local copy of tau2

--- rl_post_train/external/tau2-bench/src/tau2/domains/airline/utils.py
+++ rl_post_train/external/tau2-bench/src/tau2/domains/airline/utils.py
@@ -1,5 +1,6 @@
 from tau2.utils.utils import DATA_DIR
 
+# Local workspace note: this vendored copy is intentionally editable.
 AIRLINE_DATA_DIR = DATA_DIR / "tau2" / "domains" / "airline"
 AIRLINE_DB_PATH = AIRLINE_DATA_DIR / "db.json"
 AIRLINE_POLICY_PATH = AIRLINE_DATA_DIR / "policy.md"
